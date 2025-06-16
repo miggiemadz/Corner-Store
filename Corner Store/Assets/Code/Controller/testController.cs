64b4c7a33741c9eb0e65948d4e8caf7c456ac19c
Sidney Duverneau <sduverneau2359@gmail.com>
2025-06-16 12:32:48 -0400

    camera controller (1.5/2)
    still not working.. 😠

--- Corner Store/Assets/Code/Controller/testController.cs
+++ Corner Store/Assets/Code/Controller/testController.cs
@@ -14,11 +14,11 @@ public class testController : MonoBehaviour
     public float gravity = -9.8f; //default gravity in unity
 
     [Header("Cam Movement Variables")]
-    private float lookDirection;
+    //private float lookDirection;
     //cam sensitivity
     public float lookSensX = 1f;
     public float lookSensY = 1f;
-    //limit player's looking angle
+    //to limit player's looking angle
     public float minAngleY = -90f;
     public float maxAngleY = 90f;
 
@@ -26,17 +26,13 @@ public class testController : MonoBehaviour
     [SerializeField] Transform Camera; //reference player/main camera (or use cinemachine?)
     [SerializeField] CharacterController charController; //reference controller
 
-    //TO-DO:
-    // vars for sensitivity?
-    // add y-axis limit/look angle?
-
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         charController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked; //to lock the cursor while in game
-        Cursor.visible = false;
+        Cursor.visible = false; //hides cursor while in game
     }
 
     // Update is called once per frame
@@ -54,27 +50,33 @@ public class testController : MonoBehaviour
         //charController.Move(inputDirection * maxSpeed * Time.deltaTime);
 
 
-        //cam movement
-        if(Camera != null)
+        //Camera movement (not working yet)
+        if (Camera != null) //make sure player cam exists
         {
             float mouseX = Input.GetAxis("Mouse X"); //mouse X-axis movement (edit>projSettings>MouseX)
-            float mouseY = Input.GetAxis("Mouse Y"); //float mouseX = Input.GetAxis("Mouse X"); //mouse X-axis movement (edit>projSettings>MouseX) 
-            //lookDirection = ().normalized;
+            float mouseY = Input.GetAxis("Mouse Y");
+            //Vector3 lookDirection = new Vector3(mouseX * lookSensX, mouseY * lookSensY, ???);
+            Vector2 lookDirection = new Vector2(mouseX * lookSensX, mouseY * lookSensY); //creates a vector2 with X&Y's that correspond to user's mouse movement&sens
+            //Camera.Rotate(lookDirection); //rotate player's cam according to ^
+            //should I normalize???(ln 59) -> Vector2 lookDirection = new Vector2(mouseX * lookSensX, mouseY * lookSensY).normalized;
+            //or...
+            //transform.rotation *= Quaternion.AngleAxis(lookDirection, Vector3.up);
+            transform.localEulerAngles = lookDirection; //(using either this method or one on ln 60)
+            Camera.Rotate(lookDirection, Space.World); //use space.self or space.world?
         }
 
-        //jumping
+        //Jumping (not working yet)
         if (Input.GetButton("Jump") && charController.isGrounded) //if player is grounded & pressing spacebar/X/A, etc.
         {
-            moveDirection.y = jumpSpeed; //apply jumo force
+            moveDirection.y += jumpSpeed; //apply jump force
         }else{
-            moveDirection.y += gravity; //apply gravity force whenever player isn't grounded or actively jumping 
+            moveDirection.y += gravity; //apply gravity force whenever player isn't grounded or actively jumping (+= since gravity is negative)
         }
 
         //TO-DO: obj pickup
 
 
-        //miguel's code used 4 reference
+        //miguel's code used 4 reference (line 10, 43)
         //inputDirection = new Vector3(playerMovement.action.ReadValue<Vector2>().x, 0, playerMovement.action.ReadValue<Vector2>().y).normalized;
-
     }
 }
