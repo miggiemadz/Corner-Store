5abcd72874cdd9d35e99ef54767af8ca5cdd296d
miggiemadz <miguelt613377@gmail.com>
2025-07-10 11:36:19 -0400

    Main Menu Added
    New Skybox added
    
    Main Menu Created:
    Title + Version
    Play, Settings, & Quit Buttons
    All Social Media Buttons
    
    Added Store FBX

---
+++ Corner Store/Assets/Code/UI/MainMenu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using static System.Net.WebRequestMethods;
+
+public class MainMenu : MonoBehaviour
+{
+    private string discordURL = "https://discord.gg/hYzz9EUrPc";
+    private string instagramURL = "https://www.instagram.com/studioinfiniteloom/";
+    private string tikTokURL = "https://www.tiktok.com/@studio_infinite_loom";
+    private string xURL = "https://x.com/StudioInf_Loom";
+    private string youTubeURL = "https://www.youtube.com/@StudioInfiniteLoom-yt";
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+
+    public void OpenURL(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+    public void OpenDiscord()
+    {
+        OpenURL(discordURL);
+    }
+
+    public void OpenInstagram()
+    {
+        OpenURL(instagramURL);
+    }
+
+    public void OpenTikTok()
+    {
+        OpenURL(tikTokURL);
+    }
+
+    public void OpenX()
+    {
+        OpenURL(xURL);
+    }
+
+    public void OpenYouTube()
+    {
+        OpenURL(youTubeURL);
+    }
+}
